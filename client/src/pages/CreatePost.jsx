dab946fec2b5228edcb9d746d4de315242bed705
MoetassemCh <103120209+MoetassemCh@users.noreply.github.com>
2023-04-19 18:41:29 +0300

    Home

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {preveiw} from '../assets'
+
+
+
+const CreatePost = () => {
+  return (
+    <div>CreatePost</div>
+  )
+}
+
+export default CreatePost
